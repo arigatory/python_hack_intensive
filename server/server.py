03db18005180722a29a8db91a1ad68b43c42e7a2
Ivan MSI <arigatory@gmail.com>
2021-02-02 23:39:36 +0300

    server runs

--- server/server.py
+++ server/server.py
@@ -27,14 +27,18 @@ def auth():
     with open('users.json') as users_file:
         users = json.load(users_file)
 
+    # users_file = open('users.json')
+    # users = json.load(users_file)
+    # users_file.close()
+
     if login in users and users[login] == password:
         status_code = 200
         stats['success'] += 1
     else:
         status_code = 401
-    
+
     return Response(status=status_code)
 
 
-    if __name__ == '__main__':
-        app.run()
+if __name__ == '__main__':
+    app.run()
